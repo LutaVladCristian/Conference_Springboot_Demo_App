07840829c11b597b8d9a8d8f2aca245249cc0088
Vlad-Cristian LUTA <vlad_cristian.luta@stud.acs.upb.ro>
2024-05-30 12:50:55 +0300

    Implementing the session part of the Conference REST API.

--- src/main/java/com/app/conference/controller/SessionController.java
+++ src/main/java/com/app/conference/controller/SessionController.java
@@ -1,4 +1,46 @@
 package com.app.conference.controller;
 
+
+import com.app.conference.model.Session;
+import com.app.conference.service.SessionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/conference")
 public class SessionController {
+    @Autowired
+    private SessionService sessionService;
+
+    @GetMapping("/allSessions")
+    public List<Session> getAllSessions(){
+        return sessionService.list();
+    }
+
+    @GetMapping("/session_{id}")
+    public Session getSessionById(@PathVariable Long id){
+        return sessionService.getSessionById(id);
+    }
+
+    @DeleteMapping("/deleteSession_{id}")
+    public void deleteSession(@PathVariable Long id){
+        sessionService.deleteSessionById(id);
+    }
+
+    @PutMapping("/updateSession{id}toNewLength={newLength}")
+    public void updateSessionLenght(@PathVariable("id") Long id, @PathVariable("newLength") Integer newLength){
+        sessionService.updateSessionLength(id, newLength);
+    }
+
+    @PutMapping("/updateSession{id}toNewDescription={newName}")
+    public void updateSessionName(@PathVariable("id") Long id, @PathVariable("newName") String newName){
+        sessionService.updateSessionName(id, newName);
+    }
+
+    @PostMapping("/addSession")
+    public void addSession(@RequestBody Session session){
+        sessionService.addSession(session);
+    }
 }
--- src/main/java/com/app/conference/model/Session.java
+++ src/main/java/com/app/conference/model/Session.java
@@ -1,4 +1,20 @@
 package com.app.conference.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Table(name = "sessions")
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class Session {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long session_id;
+    private String session_name;
+    private Integer session_length;
 }
